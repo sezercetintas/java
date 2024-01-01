b5431773a7be7d204f4f95c6b7b41aa8b9225946
sezercetintas <154701651+sezercetintas@users.noreply.github.com>
2024-01-02 01:38:11 +0300

    Add files via upload

---
+++ Variables-PrimitiveDataTypes/src/TypeConversions.java
@@ -0,0 +1,5 @@
+public class TypeConversions {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Variables-PrimitiveDataTypes/src/Variables.java
@@ -0,0 +1,44 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        // Primitive Data Types
+
+        byte myByte=123;
+        short myShort=345;
+        int myInt=789;
+        long myLong=101112;
+        float myFloat= 13.4f;
+        double myDouble=15.6d;
+        char myChar='Q';
+        boolean myBoolean=true;
+
+        // Screen Printing Commands
+        System.out.println("Largest integer Value: "+ Byte.MAX_VALUE);
+        System.out.println("Smallest integer Value:"+ Byte.MIN_VALUE);
+        System.out.print("\n");
+        System.out.println("Largest short Value: "+ Short.MAX_VALUE);
+        System.out.println("Smallest short Value:"+ Short.MIN_VALUE);
+        System.out.print("\n");
+        System.out.println("Largest integer Value: "+ Integer.MAX_VALUE);
+        System.out.println("Smallest integer Value:"+ Integer.MIN_VALUE);
+        System.out.print("\n");
+        System.out.println("Largest short Value: "+ Long.MAX_VALUE);
+        System.out.println("Smallest short Value:"+ Long.MIN_VALUE);
+        System.out.print("\n");
+        System.out.println("Largest short Value: "+ Float.MAX_VALUE);
+        System.out.println("Smallest short Value:"+ Float.MIN_VALUE);
+        System.out.print("\n");
+        System.out.println("Largest short Value: "+ Double.MAX_VALUE);
+        System.out.println("Smallest short Value:"+ Double.MIN_VALUE);
+        System.out.print("\n");
+        System.out.println("My Char: "+myChar);
+        System.out.println("My Boolean Default Value: "+myBoolean);
+
+
+
+
+
+
+
+    }
+}
