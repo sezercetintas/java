f3d7d28364d712fc813dfead7d087c5cf4fd5685
sezercetintas <154701651+sezercetintas@users.noreply.github.com>
2024-01-02 23:25:30 +0300

    Arithmetic Operator Examples Created

---
+++ ArithmeticOperators/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int number1 = 20;
+        int number2 = 10;
+
+        System.out.println("Number 1 : " + number1 + " Number 2 :"+number2 + " Sum of Two Numbers " + (number1 + number2));
+        System.out.println("Number 1 : " + number1 + " Number 2 :"+number2 + " The Difference of Two Numbers " + (number1 - number2));
+        System.out.println("Number 1 : " + number1 + " Number 2 :"+number2 + " Product of Two Numbers " + (number1 * number2));
+        System.out.println("Number 1 : " + number1 + " Number 2 :"+number2 + " Quotient of Two Numbers " + ((double)number1 / (double)number2));
+        System.out.println("Number 1 : " + number1 + " Number 2 :"+number2 + " Mode of Two Numbers " + (number1 % number2) );
+    }
+}
