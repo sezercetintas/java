358cd59085f44d4a0a3a043752e9ad86d57d7f9f
sezercetintas <154701651+sezercetintas@users.noreply.github.com>
2024-01-03 08:52:28 +0300

    Equals Example 2 Done

---
+++ equalsExample2/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        Integer a = 3;
+        Integer b = 3;
+        Integer c = new Integer(3);
+
+        Boolean t = true;
+        Boolean f = false;
+
+        System.out.println(a == b); //TRUE
+        System.out.println(a == c); //FALSE
+        System.out.println(a.equals(b)); //TRUE
+        System.out.println(a.equals(c)); //TRUE
+
+        System.out.println(t == f);//FALSE
+        System.out.println(t.equals(f));//FALSE
+        System.out.println(t.equals(true));//TRUE
+    }
+}
