48855b74ce64014ff8f3faede8f3196d3308a513
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-06 22:15:33 +0300

    Switch Menu Selection Example

--- SwitchExamples/src/MenuSelectionExample.java
+++ SwitchExamples/src/MenuSelectionExample.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class MenuSelectionExample {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Menu:");
+        System.out.println("1. Option 1");
+        System.out.println("2. Option 2");
+        System.out.println("3. Option 3");
+        System.out.println("4. Exit");
+         System.out.print("Enter your choice (1-4): ");
+        int choice = scanner.nextInt();
+          switch (choice) {
+            case 1:
+                System.out.println("You selected Option 1.");
+                break;
+            case 2:
+                System.out.println("You selected Option 2.");
+                break;
+            case 3:
+                System.out.println("You selected Option 3.");
+                break;
+            case 4:
+                System.out.println("Exiting the menu.");
+                break;
+            default:
+                System.out.println("Invalid choice. Please enter a number between 1 and 4.");
+        }
 
     }
 }
