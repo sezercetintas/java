3c4c4defa4f8d4e04b8756258d739292064df30f
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-06 22:26:26 +0300

    Switch Calculator App Example

--- SwitchExamples/src/CalculatorApp.java
+++ SwitchExamples/src/CalculatorApp.java
@@ -1,5 +1,41 @@
+import java.util.Scanner;
+
 public class CalculatorApp {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+        System.out.println("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Enter the operation (+, -, *, /): ");
+        char operation = scanner.next().charAt(0);
+        double result;
+
+        switch (operation) {
+            case '+':
+                result = num1 + num2;
+                System.out.println("Result: " + result);
+                break;
+            case '-':
+                result = num1 - num2;
+                System.out.println("Result: " + result);
+                break;
+            case '*':
+                result = num1 * num2;
+                System.out.println("Result: " + result);
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println("Result: " + result);
+                } else {
+                    System.out.println("Cannot divide by zero.");
+                }
+                break;
+            default:
+                System.out.println("Invalid operation. Please enter +, -, *, or /.");
+        }
 
 
     }
