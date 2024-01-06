bb814132325abd82fab6db78a15c8ca20299acc7
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-06 22:32:57 +0300

    Switch Character Type Categorization Example

--- SwitchExamples/src/CharacterTypeCategorization.java
+++ SwitchExamples/src/CharacterTypeCategorization.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class CharacterTypeCategorization {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a character: ");
+        char character = scanner.next().charAt(0);
+
+        String category;
+
+        switch (Character.getType(character)) {
+            case Character.UPPERCASE_LETTER:
+                category = "Uppercase Letter";
+                break;
+            case Character.LOWERCASE_LETTER:
+                category = "Lowercase Letter";
+                break;
+            case Character.DECIMAL_DIGIT_NUMBER:
+                category = "Digit";
+                break;
+            default:
+                category = "Other";
+        }
+
+        System.out.println("Character category: " + category);
+
     }
 }
