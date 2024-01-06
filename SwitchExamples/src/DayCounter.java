f67fe2f725f7d5758a31d0cf9c2b550b44a9b343
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-06 23:18:11 +0300

    Update DayCounter.java

--- SwitchExamples/src/DayCounter.java
+++ SwitchExamples/src/DayCounter.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class DayCounter {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the year: ");
+        int year = scanner.nextInt();
+        System.out.print("Enter the month (1-12): ");
+        int month = scanner.nextInt();
+        int daysInMonth;
+
+        switch (month) {
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                daysInMonth = 31;
+                break;
+            case 4: case 6: case 9: case 11:
+                daysInMonth = 30;
+                break;
+            case 2:
+                if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+                    daysInMonth = 29;
+                } else {
+                    daysInMonth = 28;
+                }
+                break;
+            default:
+                System.out.println("Invalid month. Please enter a number between 1 and 12.");
+                return;
+        }
+
+        System.out.println("Number of days in the selected month: " + daysInMonth);
+
     }
 }
