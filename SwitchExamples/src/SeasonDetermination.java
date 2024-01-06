13cc0f0de25bfc304ff4b09ff8c4273931f3a02d
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-06 22:29:15 +0300

    Switch Season Determination Example

--- SwitchExamples/src/SeasonDetermination.java
+++ SwitchExamples/src/SeasonDetermination.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
 public class SeasonDetermination {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the month (1-12): ");
+        int month = scanner.nextInt();
+        System.out.print("Enter the year: ");
+        int year = scanner.nextInt();
 
+        String season;
+
+        switch (month) {
+            case 12:
+            case 1:
+            case 2:
+                season = "Winter";
+                break;
+            case 3:
+            case 4:
+            case 5:
+                season = "Spring";
+                break;
+            case 6:
+            case 7:
+            case 8:
+                season = "Summer";
+                break;
+            case 9:
+            case 10:
+            case 11:
+                season = "Fall";
+                break;
+            default:
+                season = "Invalid month";
+        }
+
+        System.out.println("Season: " + season);
     }
 }
