b801d809aa926852e00d5d7324767b0eb3675b16
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-06 22:21:53 +0300

    Switch Vowel Consonant Example (charAt&&toLowerCase)

--- SwitchExamples/src/VowelConsonantExample.java
+++ SwitchExamples/src/VowelConsonantExample.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class VowelConsonantExample {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a letter: ");
+        char letter = scanner.next().charAt(0);
+
+        switch (Character.toLowerCase(letter)) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                System.out.println("The letter is a vowel.");
+                break;
+            default:
+                System.out.println("The letter is a consonant.");
+        }
 
     }
 }
