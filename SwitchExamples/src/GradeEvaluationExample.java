efd00f8d80d6788a31fa40413f805029d57d4df4
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-06 22:13:08 +0300

    Switch Grade Evaluation Example

--- SwitchExamples/src/GradeEvaluationExample.java
+++ SwitchExamples/src/GradeEvaluationExample.java
@@ -1,5 +1,27 @@
 public class GradeEvaluationExample {
     public static void main(String[] args) {
+        int studentScore = 70;
+        char grade;
 
+        switch (studentScore / 10) {
+            case 10:
+            case 9:
+                grade = 'A';
+                break;
+            case 8:
+                grade = 'B';
+                break;
+            case 7:
+                grade = 'C';
+                break;
+            case 6:
+                grade = 'D';
+                break;
+            default:
+                grade = 'F';
+        }
+
+        System.out.println("Student score: " + studentScore);
+        System.out.println("Grade: " + grade);
     }
 }
