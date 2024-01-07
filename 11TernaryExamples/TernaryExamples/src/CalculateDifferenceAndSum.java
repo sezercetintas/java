4ed7c64ba7a28f9c4ea8e307b9a8b86f31a02ae9
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-07 22:43:39 +0300

    Update CalculateDifferenceAndSum

--- 11TernaryExamples/TernaryExamples/src/CalculateDifferenceAndSum.java
+++ 11TernaryExamples/TernaryExamples/src/CalculateDifferenceAndSum.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class CalculateDifferenceAndSum {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        int difference = (num1 > num2) ? (num1 - num2) : (num2 - num1);
+        int sum = num1 + num2;
+
+        System.out.println("Difference: " + difference);
+        System.out.println("Sum: " + sum);
     }
 }
