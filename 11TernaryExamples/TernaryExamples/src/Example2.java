003120b5187f4026039721c616a89a40deb0b8a4
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-07 22:33:10 +0300

    Ternary Example 2

---
+++ 11TernaryExamples/TernaryExamples/src/Example2.java
@@ -0,0 +1,9 @@
+public class Example2 {
+    public static void main(String[] args) {
+        int number = -7;
+
+         String result = (number > 0) ? "Positive" : "Negative or zero";
+
+        System.out.println(result);
+    }
+}
