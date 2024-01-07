93e4aafeffc45db6a6236967e86b1d8cdbcce085
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-07 22:32:09 +0300

    Ternary Example1

---
+++ 11TernaryExamples/TernaryExamples/src/Example1.java
@@ -0,0 +1,10 @@
+public class Example1 {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+
+      String result = (x < y) ? "x is less" : "y is less";
+
+        System.out.println(result);
+    }
+}
