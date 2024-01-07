0f2dc10e9ad2ac4fbde41ffa16107139ebf0d307
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-07 22:34:15 +0300

    Ternary Example 3

---
+++ 11TernaryExamples/TernaryExamples/src/Example3.java
@@ -0,0 +1,9 @@
+public class Example3 {
+    public static void main(String[] args) {
+        int num = 8;
+
+        String result = (num % 2 == 0) ? "Even" : "Odd";
+
+        System.out.println(result);
+    }
+}
