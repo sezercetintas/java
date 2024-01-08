12fd4836ce1554014f8526d0de327bcee3022c37
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-08 20:07:44 +0300

    Update FindEvenNumbers

--- 10GeneralReviewExamples 1/FindEvenNumbers/src/Main.java
+++ 10GeneralReviewExamples 1/FindEvenNumbers/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-         
+        for (int i = 1; i <100 ; i++) {
+
+            String number= (i%2==0)?i+" Even number":i+" Odd number";
+
+            System.out.println(number);
+
+        }
     }
 }
