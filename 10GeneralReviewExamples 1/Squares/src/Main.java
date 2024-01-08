fc6feb12ddc07a5048b83f01031b82a1efbba112
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-08 22:43:17 +0300

    Update Squares

--- 10GeneralReviewExamples 1/Squares/src/Main.java
+++ 10GeneralReviewExamples 1/Squares/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-      
+        int number = 1;
+        System.out.println("Squares of numbers between 1 and 50 (Included):");
+
+        do {
+            int square = number * number;
+            System.out.println("( " + number + "²): " + square);
+            number++;
+        } while (number <= 50);
     }
 }
