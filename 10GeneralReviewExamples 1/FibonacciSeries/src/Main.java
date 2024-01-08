157a45df7c9b40d5a478f055b8726147b9ea7522
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-08 22:36:48 +0300

    Update Fibonacci Series

--- 10GeneralReviewExamples 1/FibonacciSeries/src/Main.java
+++ 10GeneralReviewExamples 1/FibonacciSeries/src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-         
+        int n = 10;
+        int firstTerm = 0;
+        int secondTerm = 1;
+        int count = 0;
+
+        System.out.print("Fibonacci First 10 terms: ");
+
+        while (count < n) {
+
+            String print= (count<9)?(firstTerm + ","):(firstTerm + "");
+            System.out.print(print);
+            int nextTerm = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+
+            count++;
+        }
     }
 }
