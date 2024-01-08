49dff50fa7b05d2344f52f9f1090019bd76fc819
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-08 21:07:37 +0300

    Update MultiplesOfFive

--- 10GeneralReviewExamples 1/MultiplesOfFive/src/Main.java
+++ 10GeneralReviewExamples 1/MultiplesOfFive/src/Main.java
@@ -1,16 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         boolean key=true;
-        int i=1;
+        int i=0;
         int result;
         int temporary=0;
-        int others;
+        int others=0;
+        String text;
 
         do {
             i++;
             key=(i>=50)?(key=false):(key=true);
             result=(i%5==0)?(temporary=i):(others=i);
-            System.out.println(temporary);
+            System.out.println((result == temporary) ? "=>"+temporary : others);
 
         }
         while (key);
