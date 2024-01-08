3f97ea843736c0bf6f8c5f926f98ef980a24ea7d
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-08 20:33:31 +0300

    Update FactorialCalculator

--- 10GeneralReviewExamples 1/FactorialCalculator/src/Main.java
+++ 10GeneralReviewExamples 1/FactorialCalculator/src/Main.java
@@ -3,25 +3,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input =new Scanner(System.in);
+        System.out.println("Number: ");
         int number= input.nextInt();
         int temporary=1;
         boolean key=true;
         int result=0;
 
-
         key=(number>0 )?true:false;
-
 
         do {
 
-            temporary=(number<=1)?(temporary*=number):(temporary=number);
+            temporary=(number>=1)?(temporary*=number):(temporary=number);
             number--;
+            if (number<=1) {
+                key=false;
+            }
         }
-        while (true);
+        while (key);
 
         System.out.println(number+"!: "+temporary);
-
-
 
 
     }
