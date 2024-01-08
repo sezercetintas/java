93ea498e0cf7e3171f538261d87790ab6a20e120
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-08 20:46:24 +0300

    Update MultiplesOfFive

--- 10GeneralReviewExamples 1/MultiplesOfFive/src/Main.java
+++ 10GeneralReviewExamples 1/MultiplesOfFive/src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        
+        boolean key=true;
+        int i=0;
+        int result;
+        int temporary;
+
+        do {
+            i++;
+            key=(i>=50)?(key=false):(key=true);
+            result=(i%5==0)?(result=i):(temporary=i);
+
+            System.out.println(result);
+
+
+
+
+        }
+        while (true);
     }
 }
