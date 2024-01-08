2c861a464b6ba2469128f82e90a6a76f92889cec
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-08 22:57:52 +0300

    Update Prime Factors

--- 10GeneralReviewExamples 1/PrimeFactors/src/Main.java
+++ 10GeneralReviewExamples 1/PrimeFactors/src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-      
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        int factor = 2;
+
+        System.out.print("Prime factors of " + number + ": ");
+
+        do {
+
+            if (number % factor == 0) {
+                System.out.print(factor + " ");
+                number /= factor;
+            } else {
+                factor++;
+            }
+ 
+
+        } while (number > 1);
     }
 }
