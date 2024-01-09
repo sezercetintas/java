5afd51f17a2c86812a09c73bd3c235449a36d6d9
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-09 22:43:09 +0300

    Update SquareCalculator - Methods

--- 12Methods/SquareCalculator/src/Main.java
+++ 12Methods/SquareCalculator/src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-       
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        printSquare(number);
+    }
+
+
+    public static void printSquare(int num) {
+        int a=0,i=1;
+
+        while (i<=num)
+        {
+            a+=num;
+            i++;
+
+        }
+
+        System.out.println("("+num+"²) = " + a);
     }
 }
