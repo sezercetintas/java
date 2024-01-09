ab919b8a6c08894c60f9fc1fd6b12c2d7b5c7494
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-10 01:14:12 +0300

    Update AverageCalculator - Methods

--- 12Methods/AverageCalculator/src/Main.java
+++ 12Methods/AverageCalculator/src/Main.java
@@ -1,3 +1,5 @@
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Main {
@@ -5,20 +7,23 @@ public class Main {
 
         Scanner input=new Scanner(System.in);
         System.out.print("Enter the first number: ");
-        int myNumber1=input.nextInt();
+        double myNumber1=input.nextDouble();
         System.out.print("Enter the second number:  ");
-        int myNumber3=input.nextInt();
+        double myNumber2=input.nextDouble();
         System.out.print("Enter the third number:  ");
-        int myNumber2=input.nextInt();
+        double myNumber3=input.nextDouble();
 
         calculateAverage(myNumber1,myNumber2,myNumber3);
 
     }
 
-    public static void calculateAverage(int number1, int number2,int number3) {
-    double result=(number1+number2+number3)/3;
-        System.out.println("Average of entered numbers: " +result);
-        
+    public static void calculateAverage( Double number1, Double number2,Double number3) {
+
+//        double result=(number1+number2+number3)/3;
+//        System.out.println("Average of entered numbers: " + ((result % 1 == 0) ? (int) result : result));
+
+        BigDecimal result = BigDecimal.valueOf((number1 + number2 + number3) / 3);
+        System.out.println("Average of entered numbers: " + ((result.scale() <= 0) ? result.intValue() : result));
 
     }
 }
