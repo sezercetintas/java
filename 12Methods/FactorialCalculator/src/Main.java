ee4bd48f47bd26e8478725d95958497b735c7744
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-09 23:19:24 +0300

    Update FactorialCalculator - Methods

--- 12Methods/FactorialCalculator/src/Main.java
+++ 12Methods/FactorialCalculator/src/Main.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int number = scanner.nextInt();
 
+        printFactorial(number);
     }
 
+
     public static void printFactorial(int num) {
-         
+
+        if (num < 0) {
+            System.out.println("Factorial is undefined for negative numbers.");
+        } else {
+            long factorial = 1;
+            for (int i = 1; i <= num; i++) {
+                factorial *= i;
+            }
+            System.out.println( num + " !: " + factorial);
+        }
     }
 }
