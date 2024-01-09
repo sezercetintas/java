1361b030da88bf8943dc0bd25bae15bca3cefd4b
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-09 23:07:44 +0300

    Update ArithmeticCalculator - Methods

--- 12Methods/ArithmeticCalculator/src/Main.java
+++ 12Methods/ArithmeticCalculator/src/Main.java
@@ -1,8 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
 
+         performArithmeticOperations(num1, num2);
     }
     public static void performArithmeticOperations(double num1, double num2) {
-        
+        double sum = num1 + num2;
+        double difference = num1 - num2;
+        double product = num1 * num2;
+
+        double quotient = (num2 != 0) ? (num1 / num2) : 0;
+
+        System.out.println("Sum: " + sum);
+        System.out.println((num1>num2)?("Difference: " + difference):("Difference: " + (Math.abs(difference))));
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
     }
 }
