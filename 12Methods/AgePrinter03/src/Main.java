781ea35e58b41cb7b45c9e962bbfd77fa96044a2
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-09 22:17:54 +0300

    Update AgePrinter - Methods

--- 12Methods/AgePrinter03/src/Main.java
+++ 12Methods/AgePrinter03/src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-       
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your age:  ");
+        int age = scanner.nextInt();
+
+        printAge(age);
+    }
+    public static void printAge(int age) {
+        System.out.println("Your age: : " + age);
     }
 }
