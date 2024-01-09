3cbd2e889a4f9a486c9cdcc583009a65b8590d7d
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-09 22:31:49 +0300

    Update RandomNumberPrinter - Methods

--- 12Methods/RandomNumberPrinter04/src/Main.java
+++ 12Methods/RandomNumberPrinter04/src/Main.java
@@ -3,18 +3,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Random sayı ekrana kaç defa yazılsın? ");
-        Scanner input=new Scanner();
-        printRandomNumbers(5);
+
+        Scanner input=new Scanner(System.in);
+        System.out.println("How many random numbers should be generated? ");
+        int enteredNumber=input.nextInt();
+        printRandomNumbers(enteredNumber);
     }
 
     public static void printRandomNumbers(int count) {
         Random random=new Random();
-        System.out.print("Random Numbers: ");
-        int number= random.nextInt(100)+1;
+        System.out.println("Random Numbers: ");
 
-        for (int i = 0; i < count; i++) {
 
+        for (int i = 1; i <= count; i++) {
+            int number= random.nextInt(100)+1;
+            System.out.println(i+" ==> "+number);
         }
 
 
