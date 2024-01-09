5af96db446a266d56e26891adad8538463f33163
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-09 22:12:09 +0300

    Update PrintName - Methods

--- 12Methods/PrintName01/src/Main.java
+++ 12Methods/PrintName01/src/Main.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
 
-        
+        printName(name);
+    }
+
+    public static void printName(String name) {
+
+        System.out.println("Hello, " + name + "! Welcome!");
     }
 }
