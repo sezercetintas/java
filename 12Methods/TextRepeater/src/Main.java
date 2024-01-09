123b3bf5424a962a098afb8333accfe157809b09
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-09 22:52:49 +0300

    Update TextRepeater - Methods

--- 12Methods/TextRepeater/src/Main.java
+++ 12Methods/TextRepeater/src/Main.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a text: ");
+        String userText = scanner.nextLine();
+        System.out.print("How many times do you want to repeat the text? ");
+        int repeatCount = scanner.nextInt();
+        repeatText(userText, repeatCount);
     }
 
     public static void repeatText(String text, int count) {
+        int i=1;
+        while (i<=count)
+        {
+            System.out.println(i+". ==> "+text);
+            i++;
 
+        }
     }
 }
