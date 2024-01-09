e6e4c7595cc4129dad7835a3c83a25d6a548b0bc
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-09 22:14:46 +0300

    Update Print Message- Methods

--- 12Methods/PrintMessage00/src/Main.java
+++ 12Methods/PrintMessage00/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-         
+        printMessage();
+    }
+
+    public static void printMessage() {
+        System.out.println("Hello, this is a simple message!");
     }
 }
