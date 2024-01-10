19fe48a582b6c2c35c83ad16e1c3059c1639714a
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-10 08:36:57 +0300

    TriangleType - Methods

---
+++ 12Methods/untitled/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+
+    public static String determineTriangleType(double a, double b, double c) {
+
+        return null;
+     }
+    }
