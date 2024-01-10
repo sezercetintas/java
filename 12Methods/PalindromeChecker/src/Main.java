6435efeed116a98b33b4d390caa6f87cea31097b
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-10 10:19:23 +0300

    Update PalindromeChecker-Methods

--- 12Methods/PalindromeChecker/src/Main.java
+++ 12Methods/PalindromeChecker/src/Main.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a text: ");
+        String text = scanner.nextLine();
 
+        System.out.println((isPalindrome(text)==true)?("This text is a palindrome."):("This text is not a palindrome"));
     }
     public static boolean isPalindrome(String str) {
-      
+        String cleanText = str.toLowerCase().replaceAll("\\s", "");
+        String reversedText = new StringBuilder(cleanText).reverse().toString();
+
+        return cleanText.equals(reversedText);
     }
 }
