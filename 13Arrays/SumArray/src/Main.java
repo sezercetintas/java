17abd8757a98270295a3ad6d7f493424a5d33627
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-10 19:58:21 +0300

    Sum Array

---
+++ 13Arrays/SumArray/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[] numbers={10,20,30,40,50};
+        int sum=0;
+        int i = 0;
+
+         
+        while (i < numbers.length) {
+            sum += numbers[i];
+            i++;
+        }
+        System.out.println("Sum:" +sum);
+
+
+    }
+}
