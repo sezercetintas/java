b3d09d214f288520514acb62eb77759c75c43310
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-10 09:23:56 +0300

    Update LetterGradeConverter  Methods

--- 12Methods/LetterGradeConverter/src/Main.java
+++ 12Methods/LetterGradeConverter/src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your grade (0-100): ");
+        double grade = scanner.nextDouble();
+
+        System.out.println("Letter Grade: " + convertToLetterGrade(grade));
+
+
+    }
+    public static String convertToLetterGrade(double not) {
+        if (not < 0 || not > 100) {
+            return "Invalid grade";
+        } else if (not >= 90) {
+            return "AA";
+        } else if (not >= 80) {
+            return "BA";
+        } else if (not >= 70) {
+            return "BB";
+        } else if (not >= 60) {
+            return "CB";
+        } else if (not >= 50) {
+            return "CC";
+        } else if (not >= 40) {
+            return "DC";
+        } else {
+            return "FF";
+        }
     }
 }
