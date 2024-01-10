dafe40d0145d7753974f3e5b8509b808ff108ef1
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-10 09:01:09 +0300

    Update GradeChecker-Methods

--- 12Methods/GradeChecker/src/Main.java
+++ 12Methods/GradeChecker/src/Main.java
@@ -1,13 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Midterm Grade: ");
+        double midtermGrade = scanner.nextDouble();
+        System.out.print("Final Exam Grade: ");
+        double finalExamGrade = scanner.nextDouble();
+
+        System.out.println(isPassing(calculatePassingGrade(midtermGrade, finalExamGrade)) ? "Congratulations! You passed the course!, Your note: "+calculatePassingGrade(midtermGrade, finalExamGrade) : " Sorry, you did not pass the course, Your note: "+ calculatePassingGrade(midtermGrade, finalExamGrade));
 
     }
 
     public static double calculatePassingGrade(double midtermGrade, double finalExamGrade) {
- 
+        return (midtermGrade * 0.4) + (finalExamGrade * 0.6);
     }
 
     public static boolean isPassing(double passingGrade) {
-
+        return passingGrade >= 50;
     }
 }
