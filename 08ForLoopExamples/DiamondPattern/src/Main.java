3816b1a871378ea9d76e2cb4807ac84323e12177
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-11 23:27:26 +0300

    Update DiamondPattern - Methods

--- 08ForLoopExamples/DiamondPattern/src/Main.java
+++ 08ForLoopExamples/DiamondPattern/src/Main.java
@@ -1,8 +1,28 @@
 public class Main {
     public static void main(String[] args) {
 
+        int rows = 5;
 
-        
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i * 2 - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = rows - 1; i >= 1; i--) {
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i * 2 - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
 
     }
 }
