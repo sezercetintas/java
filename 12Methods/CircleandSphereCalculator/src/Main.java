88c059c55e6786614d29d63b27e0c7665845b70c
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-11 23:04:56 +0300

    Update CircleandSphereCalculator-Methods

--- 12Methods/CircleandSphereCalculator/src/Main.java
+++ 12Methods/CircleandSphereCalculator/src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-         
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the radius of the circle: ");
+        double circleRadius = scanner.nextDouble();
+
+        double circleArea = calculateCircleArea(circleRadius);
+        System.out.println("Area of the circle: " + circleArea);
+
+        System.out.print("Enter the radius of the sphere: ");
+        double sphereRadius = scanner.nextDouble();
+
+        double sphereVolume = calculateSphereVolume(sphereRadius);
+        System.out.println("Volume of the sphere: " + sphereVolume);
+    }
+
+
+    private static double calculateCircleArea(double radius) {
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+    private static double calculateSphereVolume(double radius) {
+        return (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
     }
 }
