44c8653163d00f1b6d4e11fe2f64cc1def106b20
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-11 23:24:23 +0300

    Update InvertedPyramidPattern-Methods

--- 08ForLoopExamples/InvertedPyramidPattern/src/Main.java
+++ 08ForLoopExamples/InvertedPyramidPattern/src/Main.java
@@ -1,7 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
+        int rows = 5;
 
+        for (int i = rows; i >= 1; i--) {
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i * 2 - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
 
     }
