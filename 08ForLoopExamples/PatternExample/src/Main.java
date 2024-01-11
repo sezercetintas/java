5e21ab22d59012f605150dc21e05d8d82571c143
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-11 23:21:05 +0300

    Update PatternExample-Methods

--- 08ForLoopExamples/PatternExample/src/Main.java
+++ 08ForLoopExamples/PatternExample/src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-         
+        int rows = 9;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 0; k < rows - i; k++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
     }
-}
+    }
