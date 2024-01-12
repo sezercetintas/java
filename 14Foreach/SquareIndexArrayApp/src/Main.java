f416f44e255e36a3b80b0dd9b58db72d80178789
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-12 21:22:19 +0300

    Update SquareIndexArrayApp - Foreach

--- 14Foreach/SquareIndexArrayApp/src/Main.java
+++ 14Foreach/SquareIndexArrayApp/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-      
+      int numbers []=new int[10];
+
+        for (int i = 0; i <numbers.length ; i++) {
+            numbers[i]=i*i;
+
+        }
+        for (int number: numbers) {
+            System.out.println(number +" ");
+        }
     }
 }
