662a511f7f21f64f5ac98f0252a6ba112fb99dae
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-12 21:07:24 +0300

    Update ReverseArrayInPlaceExample - Foreach

--- 14Foreach/ReverseArrayInPlaceExample-Foreach/src/Main.java
+++ 14Foreach/ReverseArrayInPlaceExample-Foreach/src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        for (int i = 0; i < numbers.length / 2; i++) {
+            int temp = numbers[i];
+            numbers[i] = numbers[numbers.length - 1 - i];
+            numbers[numbers.length - 1 - i] = temp;
+        }
+
+        for (int number : numbers) {
+            System.out.println(number);
+        }
     }
 }
