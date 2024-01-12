2fc3ade5bfbbfd51530059831a73e344e83b1a94
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-12 20:49:20 +0300

    Update Char Array Example - Foreach

--- 14Foreach/CharArrayExample/src/Main.java
+++ 14Foreach/CharArrayExample/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        
+        char [] characters={'S','E','Z','E','R'};
+
+        for (char word:characters){
+            System.out.print(word);
+        }
+
+
     }
 }
