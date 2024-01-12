c9364bdb3500e12e20927093c3420d4724b0b9af
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-12 21:02:38 +0300

    Update SortArrayExample - Foreach

--- 14Foreach/SortArrayExampleForeach/src/Main.java
+++ 14Foreach/SortArrayExampleForeach/src/Main.java
@@ -1,8 +1,17 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
+    int [] numbers={1,13,4,45,5,67,2};
+
+        Arrays.sort(numbers);
+
+        for (int number:numbers)
+        {
+            System.out.println(number);
+        }
 
 
-        
     }
 }
