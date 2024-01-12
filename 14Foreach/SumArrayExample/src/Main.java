3ddba9cbaaa2109aadbe95c173a0b371a4374271
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-12 20:43:11 +0300

    Sum Array Example Foreach

---
+++ 14Foreach/SumArrayExample/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int sum=0;
+        int numbers[]={1,2,3,4,5};
+
+        for (int number:numbers) {
+            sum+=number;
+
+        }
+        System.out.println(sum);
+    }
+}
