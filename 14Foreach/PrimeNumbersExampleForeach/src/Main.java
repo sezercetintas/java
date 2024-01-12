e1a6e98f30ce737a139d60a40a0af84634c19f13
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-12 21:00:01 +0300

    Update Prime Numbers Example - Foreach

--- 14Foreach/PrimeNumbersExampleForeach/src/Main.java
+++ 14Foreach/PrimeNumbersExampleForeach/src/Main.java
@@ -1,6 +1,22 @@
 public class Main {
     public static void main(String[] args) {
 
+        int[] numbers={13,34,56,76,23,12,64};
+
+        for ( int number: numbers) {
+        boolean isPrime=true;
+
+            for (int i = 2; i <number ; i++) {
+                if (number % i==0) {
+                    isPrime=false;
+                    break;
+                }
+            }
+            if(isPrime){
+                System.out.println(number + "IS PRIME!");
+            }
+        }
+
 
     }
 }
