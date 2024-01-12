b401b252d14cbd0d909e35b71f47439dda88f8ee
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-12 21:31:24 +0300

    Update MonthArrayExample

--- 14Foreach/MonthArrayExample/src/Main.java
+++ 14Foreach/MonthArrayExample/src/Main.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        
+        String[] months = {
+                "January", "February", "March", "April",
+                "May", "June", "July", "August",
+                "September", "October", "November", "December"
+        };
+        Scanner input=new Scanner(System.in);
+        System.out.print("Which Month? : ");
+        int month= input.nextInt();
 
+        System.out.println(months[month-1]);
 
     }
+
+
 }
