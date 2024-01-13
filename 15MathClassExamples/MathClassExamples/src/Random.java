361b2d74c206e798131963441bbf177bf10802bd
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-13 23:36:15 +0300

    MathClassExamples - Random

---
+++ 15MathClassExamples/MathClassExamples/src/Random.java
@@ -0,0 +1,7 @@
+public class Random {
+    public static void main(String[] args) {
+        double randomValue = Math.random();
+        System.out.println("Random Value between 0 and 1: " + randomValue);
+
+    }
+}
