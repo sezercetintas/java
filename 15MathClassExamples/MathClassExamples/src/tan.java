d92c7eb28503176a09eae26de7952e6e2022fc58
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-13 23:08:41 +0300

    Math class examples - tan

---
+++ 15MathClassExamples/MathClassExamples/src/tan.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class tan {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number to calculate the tangent: ");
+        double tanInput = scanner.nextDouble();
+        double tanResult = Math.tan(tanInput);
+        System.out.println("Tangent: " + tanResult);
+
+
+    }
+}
