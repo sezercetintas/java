1e5961277e3c4db84bcaa452701540c16a732d8c
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-25 23:23:32 +0300

    Update PasswordValidator

--- 17Regex/Regex/src/PasswordValidator.java
+++ 17Regex/Regex/src/PasswordValidator.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class PasswordValidator {
     public static void main(String[] args) {
-        
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter your password.");
+        String pwd = input.nextLine();
+
+        boolean lengthControl = pwd.length() > 7;
+        boolean spaceControl = pwd.replaceAll("[^ ]", "").isEmpty();
+        boolean upperCaseControl = pwd.replaceAll("[^A-Z]", "").length() > 0;
+        boolean lowerCaseControl = pwd.replaceAll("[^a-z]", "").length() > 0;
+        boolean digitControl = pwd.replaceAll("[^0-9]", "").length() > 0;
+
+        if (!digitControl){
+            System.out.println("The password must contain at least one digit.");
+        }
+
+        boolean isValid = lengthControl && spaceControl && upperCaseControl && lowerCaseControl && digitControl;
+
+        if (isValid){
+            System.out.println("The password is valid.");
+        }else {
+            System.out.println("The password is invalid.");
+        }
     }
 }
