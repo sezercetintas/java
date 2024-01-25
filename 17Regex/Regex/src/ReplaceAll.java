02d3bc817b8e2da69095f6354fdb8b940f2b0dac
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-25 23:14:13 +0300

    Regex - example02

--- 17Regex/Regex/src/ReplaceAll.java
+++ 17Regex/Regex/src/ReplaceAll.java
@@ -1,11 +1,15 @@
 public class ReplaceAll {
     public static void main(String[] args) {
 
+        //example01
         String s = "learn Java earn 1234 money";
 
         String s3 = s.replaceAll("[0-9]", "*");
         System.out.println(s3); //learn Java earn **** money
 
-        
+        //example02
+
+        int num = s.replaceAll("[^0-9]", "").length();
+        System.out.println(num); //4
     }
 }
