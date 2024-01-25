3a90bac5e98c385d3547c385ed29607c92e003f5
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-25 22:55:18 +0300

    Substring02 - beginIndex

---
+++ 16StringManipulations/StringManipulations01/src/Substring02.java
@@ -0,0 +1,11 @@
+public class Substring02 {
+    public static void main(String[] args) {
+        String s = "Java is easy";
+
+        String sub4 = s.substring(8);
+        System.out.println(sub4); //easy
+
+        
+
+    }
+}
