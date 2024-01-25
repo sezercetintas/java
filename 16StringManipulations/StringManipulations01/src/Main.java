5618b119f6e6bc7f346eace6280db41e02ab5511
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-25 22:41:21 +0300

    StringManipulations01 - charAt Update

--- 16StringManipulations/StringManipulations01/src/Main.java
+++ 16StringManipulations/StringManipulations01/src/Main.java
@@ -2,17 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        String s = "Java is easy";
 
-
-        char firstChar = s.charAt(0);
-        System.out.println(firstChar); //J
-
-        char lastChar = s.charAt(11); //Static Code
-        System.out.println(lastChar); //y
-
-        char lastChar2 = s.charAt(s.length()-1); //Dynamic Code
-        System.out.println(lastChar2);
 
     }
 }
---
+++ 16StringManipulations/StringManipulations01/src/Substring.java
@@ -0,0 +1,5 @@
+public class Substring {
+    public static void main(String[] args) {
+
+    }
+}
