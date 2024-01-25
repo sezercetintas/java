68dc08f9335539d67fd5d27a0c68bd380eb63060
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-25 22:52:46 +0300

    StringManipulations01 - Substring

--- 16StringManipulations/StringManipulations01/src/Substring.java
+++ 16StringManipulations/StringManipulations01/src/Substring.java
@@ -1,5 +1,18 @@
 public class Substring {
     public static void main(String[] args) {
+        String s = "Java is easy";
+
+        String sub1 = s.substring(0, 4);
+        System.out.println(sub1); //Java
+
+        String sub2 = s.substring(5, 7);
+        System.out.println(sub2); //is
+
+        String sub3 = s.substring(8, 12);
+        System.out.println(sub3); //easy
+
+
+
 
     }
 }
