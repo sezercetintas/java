7f0553425c69253341222e62b800999fa386aa17
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-25 23:26:52 +0300

    Update PriceReader

--- 16StringManipulations/StringManipulations01/src/PriceReader.java
+++ 16StringManipulations/StringManipulations01/src/PriceReader.java
@@ -1,5 +1,12 @@
 public class PriceReader {
     public static void main(String[] args) {
-        
+        String tv = "$456.99";
+        String laptop = "$875.99";
+
+        tv = tv.replace("$", "");
+        laptop = laptop.replace("$", "");
+
+        Double totalPrice = Double.valueOf(tv) + Double.valueOf(laptop);
+        System.out.println(totalPrice); //1332.98
     }
 }
