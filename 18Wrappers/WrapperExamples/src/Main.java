fb7ad6d0d432b484d22d57939b493b553d900ecc
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-26 21:30:24 +0300

    Update
    //Byte Wrapper Class

--- 18Wrappers/WrapperExamples/src/Main.java
+++ 18Wrappers/WrapperExamples/src/Main.java
@@ -4,24 +4,29 @@ public class Main {
 
 
 
-     //Integer Wrapper Class
+        //Integer Wrapper Class
         Integer number = Integer.valueOf(5);
         System.out.println(number);
 
 
-    //Double Wrapper Class:
+        //Double Wrapper Class:
         Double decimalNumber = Double.valueOf(3.14);
         System.out.println(decimalNumber);
 
 
 
-    //Character Wrapper Class:
+        //Character Wrapper Class:
         Character character = Character.valueOf('A');
         System.out.println(character);
 
         //Boolean Wrapper Class:
         Boolean status = Boolean.valueOf(true);
         System.out.println(status);
+
+        //Byte Wrapper Class:
+        Byte byteValue = Byte.valueOf((byte) 127);
+        System.out.println(byteValue);
+
 
 
     }
