db398abc3a87d3e6d6aa8d7c948c87a5929fcae6
sezercetintas <mail.sezercetintas@gmail.com>
2024-01-26 21:26:08 +0300

    Update
     //Integer Wrapper Class

---
+++ 18Wrappers/WrapperExamples/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+     //Integer Wrapper Class
+        Integer sayi = Integer.valueOf(5);
+        System.out.println(sayi);
+        
+
+
+
+
+
+    }
+}
