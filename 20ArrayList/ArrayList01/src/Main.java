4cfb30b93b968cf131a68a4456fc104272b54db5
sezercetintas <mail.sezercetintas@gmail.com>
2024-02-01 23:07:25 +0300

    Update ArrayList

--- 20ArrayList/ArrayList01/src/Main.java
+++ 20ArrayList/ArrayList01/src/Main.java
@@ -1,29 +1,14 @@
-
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
 
-    /*
-    ArrayList, dinamik bir array yapısıdır. Bu, ArrayList'in boyutunun, elemanlar ekleme veya çıkarma
-     sırasında otomatik olarak ayarlanabileceği anlamına gelir.
+ 
 
-    Gerçek hayattan bir örnek olarak, bir alışveriş listesi ArrayList olarak temsil edilebilir.
-    Alışveriş listesi, ürünlere ait isimlerin bir listesini içerir. Ürünler, String referans türleridir.
-    */
+        //ArrayList<Integer> ages = new ArrayList<Integer>(); //Java 6 ve oncesi
 
-    /*
-    1)Array'ler primitive data type'lari ve reference'lari depolayabilir ama Arraylist'ler non-primitive'leri (reference'lari) depolar
-    2)ArrayListler memory'de Array'lerden daha fazla yer kaplar
-    3)Array'ler olusturulurken bastan eleman sayisini belirtmek zorundayiz
-    4)ArrayList'ler eleman sayisinda esnektirler.
-    5) Array'leri Java niye iptal etmedi?
-    a) Array'ler cok daha hizlidir
-    b) Array'ler memory'de cok az yer kaplarlar
-    c) Bu nedenle eleman sayisi belli olan datalari depolamak icin Array'ler tercih edilir
-
-    6) Java'da generic (kapsamli) türler (<> ile belirtilen türler) için yalnızca referans veri türleri kullanılabilir. Primitive veri türleri (örneğin int, char, boolean, double, vb.) doğrudan generic türlerin içine yerleştirilemez. Bu nedenle, özel bir durumla karşılaştığınızda, bu türleri ilgili sınıflarla sarmalamanız (wrapper) gerekir.
-    */
+        ArrayList<Integer> ages = new ArrayList<>(); //Java 7 ve sonrasi - type inference - tip cikarimi
 
     }
 }
