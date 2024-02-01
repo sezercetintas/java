32887a51a95efee1f890c4092a3be236373006ad
sezercetintas <mail.sezercetintas@gmail.com>
2024-02-01 21:08:40 +0300

    Update MultiDimensionalArray

--- 19MultiDimensionalArray/MultiDimensionalArray/src/Main.java
+++ 19MultiDimensionalArray/MultiDimensionalArray/src/Main.java
@@ -2,9 +2,15 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        int[][] a = new int[3][2]; //3 distaki, 2 icteki eleman sayisi
+        a[0][0] = 5;
+        a[0][1] = 12;
+        a[1][0] = 81;
+        a[1][1] = 45;
+        a[2][0] = 123;
+        a[2][1] = 5;
 
     }
 }
 
-//19MultiDimensionalArray
+//Update MultiDimensionalArray
