315fc2863e6878f6f43a98a16bf9f50ee4c492f9
sezercetintas <mail.sezercetintas@gmail.com>
2024-02-01 23:09:24 +0300

    Update ArrayList

--- 20ArrayList/ArrayList01/src/Main.java
+++ 20ArrayList/ArrayList01/src/Main.java
@@ -4,11 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 
- 
+
 
         //ArrayList<Integer> ages = new ArrayList<Integer>(); //Java 6 ve oncesi
 
         ArrayList<Integer> ages = new ArrayList<>(); //Java 7 ve sonrasi - type inference - tip cikarimi
 
+        ages.add(9);
+        ages.add(12);
+        ages.add(5);
+
+        System.out.println(ages);
     }
 }
