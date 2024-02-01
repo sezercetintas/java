b7bcd88246d9454841c9d8bfec903537723df32c
sezercetintas <mail.sezercetintas@gmail.com>
2024-02-01 22:43:11 +0300

    Update Main.java

--- 19MultiDimensionalArray/MultidimensionalSorting/src/Main.java
+++ 19MultiDimensionalArray/MultidimensionalSorting/src/Main.java
@@ -2,7 +2,20 @@
     public static void main(String[] args) {
 
 
+        int[][] ages = {{15, 4}, {12, 43, 21}}; //==> 4 + 43 = 47
 
+        int enKucuk = ages[0][0]; //15
+        int enBuyuk = ages[0][0]; //15
+
+        for (int[] w : ages){
+            for (int k : w){
+                enKucuk = Math.min(enKucuk, k);
+                enBuyuk = Math.max(enBuyuk, k);
+            }
+        }
+        System.out.println(enKucuk); //4
+        System.out.println(enBuyuk); //43
+        System.out.println(enBuyuk + enKucuk); //47
 
         }
     }
