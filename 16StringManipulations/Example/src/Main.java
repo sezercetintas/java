29f16c5983750d73acefd2b80c14297323a121ea
sezercetintas <mail.sezercetintas@gmail.com>
2024-02-02 21:17:53 +0300

    Update Example For-StringManipulations

--- 16StringManipulations/Example/src/Main.java
+++ 16StringManipulations/Example/src/Main.java
@@ -1,6 +1,15 @@
  public class Main {
     public static void main(String[] args) {
 
-        
+        String s = "Marra";
+
+        for (int i = s.length() - 1; i >= 0;            i--) {
+
+            String c = s.substring(i, i + 1);
+
+            if (s.indexOf(c) == s.lastIndexOf(c)) {
+                System.out.println(c);
+            }
+        }
     }
 }
