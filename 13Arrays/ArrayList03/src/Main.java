c944cd032eabddcc1eaabb81d753c0c81f6d7fad
sezercetintas <mail.sezercetintas@gmail.com>
2024-02-02 22:19:51 +0300

    Update ArrayList03

--- 13Arrays/ArrayList03/src/Main.java
+++ 13Arrays/ArrayList03/src/Main.java
@@ -1,7 +1,17 @@
- public class Main {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
     public static void main(String[] args) {
 
+        List<Character> a = new ArrayList<>();
 
+        a.add('J');
+        a.add('a');
+        a.add('v');
+        a.add('a');
+        a.add('v');
+        System.out.println(a); //[J, a, v, a, v]
 
 
     }
