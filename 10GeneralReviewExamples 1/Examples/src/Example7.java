45bb008bb7c93cea14ba93d0c91bd26a2dc331d9
sezercetintas <mail.sezercetintas@gmail.com>
2024-02-02 21:31:06 +0300

    General Review Examples 7

---
+++ 10GeneralReviewExamples 1/Examples/src/Example7.java
@@ -0,0 +1,17 @@
+public class Example7 {
+    public static void main(String[] args) {
+
+        int j = 1;
+
+
+        while (j <= 10) {
+
+
+            System.out.println(j);
+
+            j++;
+
+        }
+
+    }
+}
