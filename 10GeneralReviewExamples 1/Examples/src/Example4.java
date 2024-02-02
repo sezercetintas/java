419f8f4f1e3ec4aacd5fd35c82e0bd2de830fdcf
sezercetintas <mail.sezercetintas@gmail.com>
2024-02-02 21:29:26 +0300

    General Review Examples 5

--- 10GeneralReviewExamples 1/Examples/src/Example4.java
+++ 10GeneralReviewExamples 1/Examples/src/Example4.java
@@ -22,6 +22,6 @@ public class Example4 {
             result = "";
 
         }
-        
+
     }
 }
---
+++ 10GeneralReviewExamples 1/Examples/src/Example5.java
@@ -0,0 +1,21 @@
+public class Example5 {
+    public static void main(String[] args) {
+        int i = 1;
+
+        int product = 1;
+
+        while (i <= 4) {
+
+
+
+            product = product * i;
+
+            i++;
+
+            System.out.println(product);
+
+
+
+        }
+    }
+}
