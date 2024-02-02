523882a62fd62a1f77db5d3f69b9d2f64a4486db
sezercetintas <mail.sezercetintas@gmail.com>
2024-02-02 21:28:33 +0300

    General Review Examples 5

---
+++ 10GeneralReviewExamples 1/Examples/src/Example4.java
@@ -0,0 +1,27 @@
+public class Example4 {
+    public static void main(String[] args) {
+
+        String result = "";
+
+
+
+        for (int i = 1; i < 4; i++) {
+
+
+
+            for (int j = (5 - i); j > 1; j--) {
+
+                result = result + (i + j);
+
+            }
+
+
+
+            System.out.println(result);
+
+            result = "";
+
+        }
+        
+    }
+}
