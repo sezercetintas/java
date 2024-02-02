06dc0ce1d1179b786be8dacfb7f9dbab0726be4d
sezercetintas <mail.sezercetintas@gmail.com>
2024-02-02 21:30:29 +0300

    General Review Examples 6

---
+++ 10GeneralReviewExamples 1/Examples/src/Example6.java
@@ -0,0 +1,24 @@
+public class Example6 {
+    public static void main(String[] args) {
+        int m = 10;
+
+        int n = 2;
+
+        int sum = 0;
+
+
+        while (m > n) {
+
+
+            m--;
+
+            n = n + 2;
+
+            sum = sum + m + n;
+
+
+        }
+
+
+    }
+}
