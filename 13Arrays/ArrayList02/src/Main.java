b885f0f7e7c756d17f738a36940931a430ffdc59
sezercetintas <mail.sezercetintas@gmail.com>
2024-02-02 21:51:59 +0300

    Update ArrayList02

---
+++ 13Arrays/ArrayList02/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        //Ornek 1: Bir Integer ArrayList oluşturun ve tek olanları 11 olarak olarak değiştirin
+
+        ArrayList<Integer> list =new ArrayList<>();
+        list.add(23);
+        list.add(24);
+        list.add(12);
+        list.add(9);
+        list.add(3);
+
+        System.out.println(list);
+ 
+
+
+
+    }
+}
