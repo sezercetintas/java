93216775b13ec4f333d877ffe9a09ff2993a3555
sezercetintas <mail.sezercetintas@gmail.com>
2024-02-02 21:26:46 +0300

    General Review Examples 2

---
+++ 10GeneralReviewExamples 1/Examples/src/Example2.java
@@ -0,0 +1,24 @@
+public class Example2 {
+    public static void main(String[] args) {
+
+        String s = "Marra";
+
+
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+
+
+
+            String c = s.substring(i, i + 1);
+
+
+
+            if (s.indexOf(c) == s.lastIndexOf(c)) {
+
+                System.out.println(c);
+
+            }
+
+        }
+    }
+}
