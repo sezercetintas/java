702a7d431d750c209fee385d8c7653908baeb7df
sezercetintas <mail.sezercetintas@gmail.com>
2024-02-02 21:25:37 +0300

    General Review Examples 1

---
+++ 10GeneralReviewExamples 1/Examples/src/Example1.java
@@ -0,0 +1,14 @@
+public class Example1 {
+    public static void main(String[] args) {
+        int y = 1;
+
+
+
+        for (int i = 0; i < 4; i++) {
+
+            y = y + i;
+
+        }
+
+    }
+}
