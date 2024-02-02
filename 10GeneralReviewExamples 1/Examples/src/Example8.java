3d72bae8b142dbae7728d6b630b2e79bd4d3f6f3
sezercetintas <mail.sezercetintas@gmail.com>
2024-02-02 21:31:45 +0300

    General Review Examples 8

---
+++ 10GeneralReviewExamples 1/Examples/src/Example8.java
@@ -0,0 +1,21 @@
+public class Example8 {
+    public static void main(String[] args) {
+        int num = 0;
+
+
+        do {
+
+
+            if (num % 2 == 0) {
+
+                num += 2;              // num = num + 2 demektir
+
+            }
+
+
+        } while (num < 7);
+
+
+        System.out.println(num);
+    }
+}
